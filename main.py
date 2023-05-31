f35234cd6ffb44093bdafd33180885f9e0b91b38
toscafrederiks <70904519+toscafrederiks@users.noreply.github.com>
2023-05-31 12:04:20 +0200

    PEP8 toegepast

--- main.py
+++ main.py
@@ -12,53 +12,57 @@ import pathlib
 
 def initializing_data():
     batch_size = 128
-    img_height = 180
-    img_width = 180
-    train_data_dir = "C:/Users/jaral/PycharmProjects/Image_analyses_ours/train"
+    image_height = 180
+    image_width = 180
+    train_data_dir = "C:/Users/jaral/PycharmProjects/" \
+                     "Image_analyses_ours/train"
     traindata_dir = pathlib.Path(train_data_dir)
 
-    train_ds = tf.keras.utils.image_dataset_from_directory(
+    train_dataset = tf.keras.utils.image_dataset_from_directory(
         traindata_dir,
         seed=123,
-        image_size=(img_height, img_width),
+        image_size=(image_height, image_width),
         batch_size=batch_size)
 
-    validation_data_dir = "C:/Users/jaral/PycharmProjects/Image_analyses_ours/val"
+    validation_data_dir = "C:/Users/jaral/PycharmProjects/" \
+                          "Image_analyses_ours/val"
     validatedata_dir = pathlib.Path(validation_data_dir)
-    val_ds = tf.keras.utils.image_dataset_from_directory(
+    val_dataset = tf.keras.utils.image_dataset_from_directory(
         validatedata_dir,
         seed=123,
-        image_size=(img_height, img_width),
+        image_size=(image_height, image_width),
         batch_size=batch_size)
 
-    class_names = train_ds.class_names
+    classes_names = train_dataset.class_names
 
-    print(class_names)
-    return train_ds, val_ds, class_names, img_height, img_width
+    print(classes_names)
+
+    return train_dataset, val_dataset, \
+        classes_names, image_height, image_width
 
 
-def pre_processing(training_data, validation_data, class_names,
-                   img_height, img_width):
-    AUTOTUNE = tf.data.AUTOTUNE
+def pre_processing(training_dataset, validation_dataset, classes_names,
+                   image_height, image_width):
+    autotune = tf.data.AUTOTUNE
 
-    train_ds = training_data.cache().shuffle(1000).prefetch(
-        buffer_size=AUTOTUNE)
-    val_ds = validation_data.cache().prefetch(buffer_size=AUTOTUNE)
+    train_data = training_dataset.cache().shuffle(1000).prefetch(
+        buffer_size=autotune)
+    val_data = validation_dataset.cache().prefetch(buffer_size=autotune)
 
     normalization_layer = layers.Rescaling(1. / 255)
-    normalized_ds = train_ds.map(
+    normalized_ds = train_data.map(
         lambda x, y: (normalization_layer(x), y))
     image_batch, labels_batch = next(iter(normalized_ds))
     first_image = image_batch[0]
     print(np.min(first_image), np.max(first_image))
 
-    num_classes = len(class_names)
+    classes_numbers = len(classes_names)
 
-    data_augmentation = keras.Sequential(
+    augmentation_data = keras.Sequential(
         [
             layers.RandomFlip("horizontal",
-                              input_shape=(img_height,
-                                           img_width,
+                              input_shape=(image_height,
+                                           image_width,
                                            3)),
             layers.RandomRotation(0.1),
             layers.RandomZoom(0.1),
@@ -66,18 +70,18 @@ def pre_processing(training_data, validation_data, class_names,
     )
 
     plt.figure(figsize=(10, 10))
-    for images, _ in train_ds.take(1):
+    for images, _ in train_data.take(1):
         for i in range(9):
-            ax = plt.subplot(3, 3, i + 1)
             plt.axis("off")
         plt.show()
-    return train_ds, val_ds, num_classes, data_augmentation
+    return train_data, val_data, classes_numbers, augmentation_data
 
 
-def model_training(train_ds, val_ds, num_classes, data_augmentation):
+def model_training(train_dataset, val_dataset, number_classes,
+                   augmentation_data):
     # Create a learning rate scheduler callback
     model = Sequential([
-        data_augmentation,
+        augmentation_data,
         layers.Rescaling(1. / 255),
         layers.Conv2D(16, 3, padding='same', activation='relu'),
         layers.MaxPooling2D(),
@@ -88,7 +92,7 @@ def model_training(train_ds, val_ds, num_classes, data_augmentation):
         layers.Dropout(0.2),
         layers.Flatten(),
         layers.Dense(128, activation='relu'),
-        layers.Dense(num_classes, name="outputs")
+        layers.Dense(number_classes, name="outputs")
 
     ])
     model.compile(optimizer='adam',
@@ -98,8 +102,8 @@ def model_training(train_ds, val_ds, num_classes, data_augmentation):
     model.summary()
     epochs = 15
     history = model.fit(
-        train_ds,
-        validation_data=val_ds,
+        train_dataset,
+        validation_data=val_dataset,
         epochs=epochs,
     )
     acc = history.history['accuracy'][2:]
@@ -135,14 +139,16 @@ def auc_score():
     Calculates the roc curve based on the saved keras model.
     """
     # Path to test directory
-    test_path = "C:/Users/jaral/PycharmProjects/Image_analyses_ours/test"
+    test_path = "C:/Users/jaral/PycharmProjects/" \
+                "Image_analyses_ours/test"
     directory_test_list = os.listdir(test_path)
     # Declare lists
     test_dataset_y_values = []
     predictions = []
     # Declare variables
-    img_height = 180
-    img_width = 180
+    image_height = 180
+    image_width = 180
+    y = 0
 
     # Get saved model
     model_interpreter = tf.lite.Interpreter(
@@ -154,26 +160,28 @@ def auc_score():
     input_details_model = model_interpreter.get_input_details()
     output_details_model = model_interpreter.get_output_details()
 
-    # Loops through test directory to give 1 point to tumor pictures
+    # Loops through test directory to give 0 points for buffalo,
+    # 1 point for elephant, 2 point for rhino or 3 points for zebra
     for sub_directory in directory_test_list:
         if 'Buffalo' in str(sub_directory):
-            tumor = 0
+            animal = 0
         elif 'Elephant' in str(sub_directory):
-            tumor = 1
+            animal = 1
         elif 'Rhino' in str(sub_directory):
-            tumor = 2
+            animal = 2
         elif 'Zebra' in str(sub_directory):
-            tumor = 3
+            animal = 3
         else:
-            tumor = -1
+            animal = -1
         # Updates the directory path
         sub_directory_full_path = test_path + '/' + sub_directory
-        # Loops through all the images in the test directory and gives them a score
+        # Loops through all the images in the test directory
+        # and gives them a score
         for file_image in os.listdir(sub_directory_full_path):
             image_path = sub_directory_full_path + '/' + file_image
             # Prepares images for testing model
             img = tf.keras.utils.load_img(image_path, target_size=(
-                img_height, img_width))
+                image_height, image_width))
             img_array = tf.keras.utils.img_to_array(img)
             img_array = tf.expand_dims(img_array, 0)
             # Put the trained model against images
@@ -184,7 +192,7 @@ def auc_score():
                 output_details_model[0]['index'])
             # Calculates score
             scores = tf.nn.softmax(output[0])
-            test_dataset_y_values.append(tumor)
+            test_dataset_y_values.append(animal)
             y = label_binarize(test_dataset_y_values,
                                classes=[0, 1, 2, 3, 4])
 
@@ -194,12 +202,13 @@ def auc_score():
     except ValueError:
         pass
     # Calculates auc_score
-    auc_score = roc_auc_score(y, predictions, multi_class='ovr')
-    print(auc_score)
+    auc_score_model = roc_auc_score(y, predictions, multi_class='ovr')
+    print(auc_score_model)
 
 
 if __name__ == '__main__':
-    training_data, validation_data, class_names, img_height, img_width = initializing_data()
+    training_data, validation_data, \
+        class_names, img_height, img_width = initializing_data()
     train_ds, val_ds, num_classes, data_augmentation = pre_processing(
         training_data, validation_data, class_names, img_height,
         img_width)
